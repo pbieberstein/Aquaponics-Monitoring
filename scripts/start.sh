27177d7e27b6c82e892ab5af0200025e9cdbdf1b
Philipp v. Bieberstein <pbieberstein@email.arizona.edu>
2016-07-22 15:18:43 +0200

    small fixes

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,10 +3,14 @@
 # No password needed because I set up ssh key pair in the host server and Raspi
 
 
-sudo python start.py
+sudo python monitor.py &&
+echo <<<started python script>>>
 
 while [ 1 ]
 do
+    echo Downloading Database
     scp -P13332 paja@160.217.215.233:/home/paja/WWW/sql_www_ap.sqlite ../data/
-    sleep 10
+    echo Download complete
+    echo sleeping for 1 minute... thank you
+    sleep 60
 done
