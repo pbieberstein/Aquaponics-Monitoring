64d01d438204774f5628a0f526e0653df37b4e8c
Philipp v. Bieberstein <pbieberstein@email.arizona.edu>
2016-07-22 11:51:55 +0200

    path fix

--- scripts/pull_sql.sh
+++ scripts/pull_sql.sh
@@ -2,4 +2,4 @@
 
 # No password needed because I set up ssh key pair in the host server and Raspi
 
-scp -P13332 paja@160.217.215.233:/home/paja/WWW/sql_www_ap.sqlite ../data/.
+scp -P13332 paja@160.217.215.233:/home/paja/WWW/sql_www_ap.sqlite ../data/
